29b4edf3fd648ea52982b5babf4028482cfb8678
agawlik <artur.gawlik@icloud.com>
2024-04-09 18:07:50 +0200

    read mask bit, mask, payload, and finall decode

--- server.js
+++ server.js
@@ -19,7 +19,8 @@ httpServer.on("upgrade", (req, socket, head) => {
   socket.write("\r\n");
 
   socket.on("data", (data) => {
-    decodeFrame(data);
+    const message = decodeFrame(data);
+    console.log(message);
   });
 });
 
@@ -32,8 +33,66 @@ httpServer.listen(80, () => {
  * @param {import("node:buffer").Buffer} data is ArrayBuffer in a Network (Unix) byte order, which means BigEndian notation (most significat byte first).
  */
 function decodeFrame(data) {
-  const contentLength = readContentLength(data);
-  console.log("content length: ", contentLength);
+  const { byteOffsetForNextReads, extractedLength } = readContentLength(data);
+  const maskBit = readMaskBit(data);
+  if (maskBit) {
+    const maskKey = readMaskKey(data, byteOffsetForNextReads);
+    const encodedPayload = readEncodedPayload(
+      data,
+      extractedLength,
+      byteOffsetForNextReads + 4
+    );
+
+    // Perform an XOR on the mask
+    const decodedPayload = Uint8Array.from(
+      encodedPayload,
+      (elt, i) => elt ^ maskKey[i % 4]
+    );
+    const textEncoder = new TextDecoder();
+    return textEncoder.decode(decodedPayload);
+  }
+  // console.log("data: ", data);
+}
+
+/**
+ * Decodes payload from frame
+ * @param {import("node:buffer").Buffer} data
+ */
+function readEncodedPayload(data, length, byteOffset) {
+  // mask bit is encoded in 8 bit
+  const dataView = new DataView(data.buffer);
+  const encodedPayload = [];
+  for (let index = 0; index < length; index++) {
+    encodedPayload.push(dataView.getUint8(byteOffset + index));
+  }
+
+  return encodedPayload;
+}
+
+/**
+ * Decodes mask key from frame
+ * @param {import("node:buffer").Buffer} data
+ */
+function readMaskKey(data, byteOffset) {
+  const dataView = new DataView(data.buffer);
+  // const maskKey = dataView.getUint32(12);
+  const maskKey = [];
+  for (let index = 0; index < 4; index++) {
+    maskKey.push(dataView.getUint8(byteOffset + index));
+  }
+
+  return maskKey;
+}
+
+/**
+ * Decodes mask bit from frame
+ * @param {import("node:buffer").Buffer} data
+ */
+function readMaskBit(data) {
+  // mask bit is encoded in 8 bit
+  const dataView = new DataView(data.buffer);
+  const maskBit = dataView.getUint8(1) & 0b00000001;
+  return maskBit;
 }
 
 /**
@@ -48,16 +107,18 @@ function readContentLength(data) {
   const secondByte = dataView.getUint8(1);
   // extract bits 1-7, which are originally bits 9-15
   let extractedLength = secondByte & 0b01111111;
-
+  let byteOffsetForNextReads = 2;
   if (extractedLength === 126) {
     // for this case need to read 16-31 bits
     extractedLength = dataView.getUint16(2);
+    byteOffsetForNextReads = 4; // TODO: probablly wrong
   } else if (extractedLength === 127) {
     // for this case need to read 12-63 bits
     extractedLength = dataView.getBigUint64(4);
+    byteOffsetForNextReads = 6; // TODO: probablly wrong
   }
 
-  return extractedLength;
+  return { extractedLength, byteOffsetForNextReads };
 }
 
 /**
